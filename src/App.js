6d0d4348b1bebfdd2439a66c89253febeee3db3b
Leandro Mello <leandrogalaxys3mini@gmail.com>
2020-02-14 21:24:52 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useMemo, useCallback} from 'react';
+
 
 function App() {
+  
+  const [listTec, setListTec] = useState([
+  ]);
+  
+  let lastID = localStorage.getItem("lastID") ? parseInt(localStorage.getItem("lastID")) : 1
+   
+ console.log('Last => ', lastID)
+  
+  const [dado, setDado] = useState({
+    id: lastID,
+    value: ''
+  });
+  
+ 
+  
+ const handleAdd = useCallback(() => {
+  setListTec([...listTec,dado])
+  setDado({
+    value: '',
+  });
+  let proxId = lastID + 1
+
+  localStorage.setItem("lastID", JSON.stringify(proxId))
+ }, [dado,listTec])
+ 
+  //ComponentDidMount
+  useEffect(() => {
+    const tech = localStorage.getItem('tech');
+    if(tech) {
+      setListTec(JSON.parse(tech))
+    }
+  },[])
+  
+
+  //ComponentDidUpdate 
+  useEffect(() => {
+    
+    localStorage.setItem('tech', JSON.stringify(listTec))
+    
+  },[listTec])
+  const techSize = useMemo(() => listTec.length, [listTec])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ul>
+      {listTec.map(element => (
+        <li key={element.id}>{element.value}</li>
+      ))}
+      
+    </ul>
+      <strong>Você tem {techSize} tecnologias</strong>
+      <br/>
+    <input type="text" onChange={event => setDado({id: lastID , value: event.target.value})} value={dado.value}/>
+    <button onClick={handleAdd}>Adicionar</button>
+    </>
   );
 }
 
